05c067202202346b64aa4cbfc541d51733946ea0
LAPTOP-O7FQJTLF\88691 <ken.lin@devpro.com.tw>
2025-09-16 11:40:52 +0800

    feat:計數器

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
 // app.component.ts
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, signal } from '@angular/core';
+import { Subject } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,54 @@ import { Component, OnInit, signal } from '@angular/core';
 export class App implements OnInit {
   title = signal('rxjs-practice');
 
+  counter = 0;
+  counter$!: Subject<number>;
+
+  @ViewChild('currentCounter') currentCounterLabel!: ElementRef;
+  @ViewChild('evenCounter') evenCounterLabel!: ElementRef;
+  @ViewChild('status') statusLabel!: ElementRef;
 
   ngOnInit(): void {}
+
+  start() {
+    this.counter$ = new Subject();
+    this.counter = 0;
+    this.statusLabel.nativeElement.innerHTML = '開始計數';
+
+    // 只取偶數的 Observable
+    const evenCounter$ = this.counter$.pipe(filter(data => data % 2 === 0));
+
+    // 訂閱主 Subject
+    this.counter$.subscribe({
+      next: (data) => {
+        this.currentCounterLabel.nativeElement.innerHTML = data.toString();
+      },
+      error: (message) => {
+        this.statusLabel.nativeElement.innerHTML = `錯誤 -> ${message}`;
+      },
+      complete: () => {
+        this.statusLabel.nativeElement.innerHTML = '完成';
+      },
+    });
+
+    // 訂閱偶數 Subject
+    evenCounter$.subscribe(data => {
+      this.evenCounterLabel.nativeElement.innerHTML = data.toString();
+    });
+
+    this.counter$.next(this.counter);
+  }
+
+  count() {
+    this.counter$.next(++this.counter);
+  }
+
+  error() {
+    const reason = prompt('請輸入錯誤訊息');
+    this.counter$.error(reason || 'error');
+  }
+
+  complete() {
+    this.counter$.complete();
+  }
 }
